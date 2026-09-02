edeeea7f66e35b1710978e2c716edfe9101c0b68
조서윤 <heu0303@naver.com>
2026-09-02 13:51:32 +0900

    feat: 헤더 컴포넌트 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,31 @@
+import { Search, UserRound, Menu } from "lucide-react";
 import styles from "./Header.module.css";
+import logoImg from "../assets/logo.png";
 
 function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}></div>
+      <img src={logoImg} className={styles.logo} />
+
+      <nav className={styles.nav}>
+        <a href="/lab">연구실</a>
+        <a href="#">교수님 정보</a>
+        <a href="#">전공 강의평</a>
+        <a href="#">소통바다</a>
+      </nav>
+
+      <div className={styles.rightMenu}>
+        <span className={styles.my}>MY</span>
+        <button type="button" className={styles.iconButton}>
+          <Search size={24} />
+        </button>
+        <button type="button" className={styles.iconButton}>
+          <UserRound size={24} />
+        </button>
+        <button type="button" className={styles.iconButton}>
+          <Menu size={27} />
+        </button>
+      </div>
     </header>
   );
 }
