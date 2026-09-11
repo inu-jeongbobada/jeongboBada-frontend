b5c608ccf5a91061cef46b29be07f4e6e1466747
조서윤 <heu0303@naver.com>
2026-09-11 18:15:40 +0900

    feat: 회원가입 UI 구현

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
+import Signup from "../pages/auth/Signup";
 import Login from "../pages/auth/Login";
 import Lab from "../pages/Lab";
 import Professor from "../pages/Professor";
@@ -12,6 +13,7 @@ function Router() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/lab" element={<Lab />} />
       <Route path="/professor" element={<Professor />} />
       <Route path="/professor/:id" element={<ProfessorDetail />} />
